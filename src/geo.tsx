ea29a2d42a5545cc7769e3fb6097333851fefd58
Jumner ArchBtw <1frankjus@hdsb.ca>
2022-03-03 22:08:49 -0500

    getting somewhere now

--- src/geo.tsx
+++ src/geo.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import * as React from "react";
+import { useEffect } from "react";
 import Geogebra from "react-geogebra";
 
+declare global {
+	interface Window {
+		app1: any;
+	}
+}
+
 export default function Geo() {
-	useEffect(() => {
-		console.log("Geo instance destroyed");
-	}, []);
 	return (
 		<Geogebra 
 			appName="3d"
@@ -12,7 +16,14 @@ export default function Geo() {
 			height={600}
 			showToolBar={false}
 			showMenuBar={false}
-			appletOnLoad={() => {console.log("Geo Instance")}}
+			appletOnLoad={() => {
+				console.log("Geo Instance");
+				setInterval(() => {
+					if (window.app1) {
+						console.log(window.app1.getBase64());
+					}
+				}, 1000);
+			}}
 			id="app1"
 		/>
 	);
